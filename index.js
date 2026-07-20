bfee67f5efe27516f43e7b663dfa7665cbe457d5
MAT-ash <silvanaalbarran5@gmail.com>
2026-07-20 01:51:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -150,8 +150,8 @@ const client = new Client({
     authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    executablePath: '/usr/bin/google-chrome-stable',
     args: ['--no-sandbox', '--disable-setuid-sandbox']
+  }
   }
 });
 
